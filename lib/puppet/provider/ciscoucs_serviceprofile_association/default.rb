283cdac1d93dfed2a882426b757a6f0f8eb6973a
Ramesh Jha <Ramesh_Jha@dell.com>
2013-12-19 12:03:28 +0000

    associate

--- lib/puppet/provider/ciscoucs_serviceprofile_association/default.rb
+++ lib/puppet/provider/ciscoucs_serviceprofile_association/default.rb
@@ -1,69 +1,63 @@
 require 'pathname'
 provider_path = Pathname.new(__FILE__).parent.parent
+Puppet.debug provider_path
 require File.join(provider_path, 'ciscoucs')
 
 Puppet::Type.type(:ciscoucs_serviceprofile_associate).provide(:default, :parent => Puppet::Provider::CiscoUCS) do
   @doc = "Manage association of service profile on Cisco UCS device."
 
   def create
-      self.transport
-      @transport
+    self.transport
+    @transport
     name=resource[:name]
-
-    request_xml = '<configConfMos cookie="' + @transport + '"inHierarchical="false">
-        <inConfigs>
-    <pair key="org-root/ls-test">
-        <lsServer
-        agentPolicyName=""
-        biosProfileName=""
-        bootPolicyName=""
-        
-        descr=""
-        dn="org-root/ls-test"
-        dynamicConPolicyName=""
-        extIPPoolName="ext-mgmt"
-        extIPState="none"
-        hostFwPolicyName=""
-        identPoolName=""
-        localDiskPolicyName=""
-        maintPolicyName=""
-        mgmtAccessPolicyName=""
-        mgmtFwPolicyName=""
-        policyOwner="local"
-        powerPolicyName="default"
-        
-        scrubPolicyName=""
-        solPolicyName=""
-        srcTemplName=""
-        statsPolicyName="default"
-        status="created,modified"
-        usrLbl=""
-        uuid="0"
-        vconProfileName="">
-            <lsRequirement
-            
-            
-            name="' + name + '" qualifier=""
-            restrictMigration="no"
-            rn="pn-req"
-            >
-            </lsRequirement>
-            <lsPower
-            rn="power"
-            state="admin-up"
-            >
-            </lsPower>
-        </lsServer>
-    </pair>
-        </inConfigs>
-    </configConfMos>'
-
-
-      puts request_xml      
-      response_xml = RestClient.post url, request_xml, :content_type => 'text/xml'      
       
-      puts "Server profile associate- " + response_xml
-    end
+    @profile_associate_input_xml =
+    '<configConfMos
+          cookie="'+cookie+'"
+          inHierarchical="false">
+              <inConfigs>
+          <pair key="org-'+organization_name+'/ls-'+service_profile_name+'">
+              <lsServer
+              agentPolicyName=""
+              biosProfileName=""
+              bootPolicyName=""
+              descr=""
+              dn="org-'+dn_organization_name+'/ls-'+dn_service_profile_name+'"
+              dynamicConPolicyName=""
+              extIPPoolName="ext-mgmt"
+              extIPState="none"
+              hostFwPolicyName=""
+              identPoolName=""
+              localDiskPolicyName=""
+              maintPolicyName=""
+              mgmtAccessPolicyName=""
+              mgmtFwPolicyName=""
+              policyOwner="local"
+              powerPolicyName="default"
+
+              scrubPolicyName=""
+              solPolicyName=""
+              srcTemplName=""
+              statsPolicyName="default"
+              status="modified"
+              usrLbl=""
+              uuid="0"
+              vconProfileName="">
+                  <lsBinding
+                  pnDn="sys/'+server_chesis_id+'/'+server_slot+'"
+                  restrictMigration="no"
+                  rn="pn"
+                  >
+                  </lsBinding>
+              </lsServer>
+          </pair>
+              </inConfigs>
+          </configConfMos>';
+    puts @profile_associate_input_xml;
+    @profile_associate_output_xml = RestClient.post @url, @profile_associate_input_xml, :content_type => 'text/xml';
+    puts "Server profile associate- " + @profile_associate_output_xml;
+    
+  end
   
     def destroy
         self.transport
