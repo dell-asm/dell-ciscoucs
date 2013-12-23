e90e4743143adb5c7a74806ed773c97c68e696d5
Ramesh Jha <Ramesh_Jha@dell.com>
2013-12-23 07:23:58 +0000

    Disassociate Server Profile PP file

---
+++ tests/disassociate_profile.pp
@@ -0,0 +1,20 @@
+include ciscoucs
+
+transport { 'ciscoucs':
+  username => 'admin',
+  password => 'admin',
+  server   => '192.168.145.130',
+}
+
+
+ciscoucs_serviceprofile_association { 'name':
+  ensure    => absent, 
+  organizationname => 'root',
+  serviceprofilename => 'DemoTestServiceProfile',
+  dnorganizationname => 'root',
+  dnserviceprofilename => 'DemoTestServiceProfile',
+  serverchesisid => 'chassis-1',
+  serverslot => 'blade-3',   
+  transport  => Transport['ciscoucs'],
+}
+
