0535a06ba7d69c50f77354630c2e7f18804ee698
Ramesh Jha <Ramesh_Jha@dell.com>
2013-12-24 05:56:04 +0000

    minor changes

--- lib/puppet/type/ciscoucs_serviceprofile_association.rb
+++ lib/puppet/type/ciscoucs_serviceprofile_association.rb
@@ -1,51 +1,37 @@
 Puppet::Type.newtype(:ciscoucs_serviceprofile_association) do
   @doc = 'Associate service profile Associate on cisco ucs device'
-  
+
   ensurable do
-      newvalue(:present) do
-        provider.create
-      end
-  
-      newvalue(:absent) do
-        provider.destroy
-      end
-  
-      defaultto(:present)
-    end 
+    newvalue(:present) do
+      provider.create
+    end
+
+    newvalue(:absent) do
+      provider.destroy
+    end
+
+    defaultto(:present)
+  end
 
   newparam(:name, :namevar => true) do
-     desc 'server profile Parameter'
-     newvalues(/\w/)
-   end
-   
+  end
+
   newparam(:organizationname) do
-       desc 'server profile Parameter'
-       newvalues(/\w/)
-     end
+  end
 
   newparam(:serviceprofilename) do
-         desc 'server profile Parameter'
-         newvalues(/\w/)
-       end
-       
+  end
+
   newparam(:dnorganizationname) do
-         desc 'server profile Parameter'
-         newvalues(/\w/)
-       end
-       
+  end
+
   newparam(:dnserviceprofilename) do
-         desc 'server profile Parameter'
-         newvalues(/\w/)
-       end
-       
+  end
+
   newparam(:serverchesisid) do
-         desc 'server profile Parameter'
-         newvalues(/\w/)
-       end
-       
+  end
+
   newparam(:serverslot) do
-           desc 'server profile Parameter'
-           newvalues(/\w/)
-         end
-         
+  end
+
 end
