fcd40b0e67781da14b7f5149b89b2fae6e5711c9
richa sharma6 <richa_sharma6@dell.com>
2013-12-27 10:31:16 +0000

    Incorporated new xml changes JAXP

--- lib/puppet/provider/ciscoucs_serviceprofile_association/default.rb
+++ lib/puppet/provider/ciscoucs_serviceprofile_association/default.rb
@@ -10,60 +10,44 @@ Puppet::Type.type(:ciscoucs_serviceprofile_association).provide(:default, :paren
   include PuppetX::Puppetlabs::Transport
   @doc = "Associate server profile on Cisco UCS device."
   def create
-    name=resource[:name]
-    serverchesisid = resource[:serverchesisid]
-    serverslot = resource[:serverslot]      
-    profile_dn = resource[:profile_dn]    
-    server_dn = resource[:server_dn]
-
-    @profile_associate_input_xml =
-    '<configConfMos cookie="'+cookie+'" inHierarchical="false">
-        <inConfigs>
-            <pair key="'+profile_dn+'">
-                <lsServer dn="'+profile_dn+'">
-                    <lsBinding pnDn="'+server_dn+'" rn="pn">
-                    </lsBinding>
-                </lsServer>
-            </pair>
-        </inConfigs>
-    </configConfMos>';
-
-    @profile_associate_output_xml = RestClient.post url, @profile_associate_input_xml, :content_type => 'text/xml';
     
-    puts @profile_associate_output_xml;
+    #@serverchesisid = resource[:serverchesisid]
+    @pndn = resource[:pndn]
+    #@serverslot = resource[:serverslot]      
+    @serviceprofilename = resource[:serviceprofilename]   
+
+      
+    formatter = PuppetX::Util::Ciscoucs::Xml_formatter.new("associateServiceProfile")
+    parameters = PuppetX::Util::Ciscoucs::NestedHash.new
+
+    puts "---------befor xml"
+    parameters['/configConfMos'][:cookie] = cookie
+    parameters['/configConfMos'][:inHierarchical] = 'false'
+    parameters['/configConfMos/inConfigs/pair'][:key] = @serviceprofilename
+    parameters['/configConfMos/inConfigs/pair/lsBinding'][:dn] = @serviceprofilename
+    parameters['/configConfMos/inConfigs/pair/lsBinding'][:pnDn] = @pndn
+    parameters['/configConfMos/inConfigs/pair/lsBinding'][:restrictMigration] = 'no'
+      
+    profile_associate_input_xml = formatter.command_xml(parameters)
+      
+    profile_associate_output_xml = post profile_associate_input_xml;
+    
+    assoresponse = REXML::Document.new(profile_associate_output_xml)
+    root = assoresponse.root
+    puts assoresponse
+      
+
 
   end
 
   def destroy
 
-    name=resource[:name]      
-    profile_dn = resource[:profile_dn]
-
-    @profile_disassociate_input_xml =
-    '<configConfMos cookie="'+cookie+'" inHierarchical="false">
-         <inConfigs>
-            <pair key="'+profile_dn+'">
-                <lsBinding dn="'+profile_dn+'" status="deleted" >
-                 </lsBinding>
-            </pair>
-          </inConfigs>
-        </configConfMos>';
-
-    @profile_disassociate_output_xml = RestClient.post url, @profile_disassociate_input_xml, :content_type => 'text/xml';
-
-    puts @profile_disassociate_output_xml;
+    
   end
 
   #check If exist
   def exists?
-    ens = resource[:ensure]
-    result = false;
-    if (ens.to_s =="present")
-      result = false;
-    elsif (ens.to_s =="absent")
-      result = true;
-    end
-    return result;
+
   end
 
 end
