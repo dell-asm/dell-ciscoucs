0f854092cc5066c4d74f16624a666cac49c83436
Ramesh Jha <Ramesh_Jha@dell.com>
2013-12-27 10:56:46 +0000

    updated DN validation

--- lib/puppet/type/ciscoucs_serviceprofile_association.rb
+++ lib/puppet/type/ciscoucs_serviceprofile_association.rb
@@ -1,5 +1,10 @@
 Puppet::Type.newtype(:ciscoucs_serviceprofile_association) do
   @doc = 'Associate service profile Associate on cisco ucs device'
+  
+  @organization_name = '';
+  @service_profile_name = '';
+  @server_chesis_id = '';
+  @server_slot = '';
 
   ensurable do
     newvalue(:present) do
@@ -14,26 +19,84 @@ Puppet::Type.newtype(:ciscoucs_serviceprofile_association) do
   end
 
   newparam(:name, :namevar => true) do
+    puts "Name of associate server profile";
   end
 
   newparam(:organization_name) do
-  end
-
-  newparam(:service_profile_name) do
-  end
+      desc "Organization Name of service profile"
+      validate do |value|
+        if value.strip.length != 0
+      #    unless value =~ /\A[a-zA-Z0-9\d_\.\:]+\Z/
+       #     raise ArgumentError, "%s is not a valid value." % value
+        #  end
+        end
+      end
+    end
   
-  newparam(:profile_dn) do
-    defaultsto 'org-'+resource[:organization_name]+'/ls-'+resource[:service_profile_name];
-  end
-
+    newparam(:service_profile_name) do
+      desc "Name of service profile"
+      validate do |value|
+        if value.strip.length != 0
+         # unless value =~ /\A[a-zA-Z0-9\d_\.\:]+\Z/
+          #  raise ArgumentError, "%s is not a valid value." % value
+         # end
+        end
+      end
+    end
+  
+    newparam(:profile_dn) do
+      desc "Name of service profile"
+      validate do |value|
+        if value.strip.length == 0
+          # if dn is empty then both org or profile name should exists.
+          if resource[:organization_name].strip.length == 0 || resource[:service_profile_name].strip.length == 0
+            raise ArgumentError, "Either dn or both org and profile name should be given in input."
+          end
+        else
+          #unless value =~ /\A[a-zA-Z0-9\d_\.\:]+\Z/
+           # raise ArgumentError, "%s is not a valid value." % value
+          #end
+        end
+      end
+    end
+  
+  
   newparam(:server_chesis_id) do
-  end
-
-  newparam(:server_slot) do
-  end
-  
-  newparam(:server_dn) do
-    defaultsto 'sys/'+resource[:server_chesis_id]+'/'+resource[:server_slot];
-  end
+        desc "Organization Name of service profile"
+        validate do |value|
+          if value.strip.length != 0
+        #    unless value =~ /\A[a-zA-Z0-9\d_\.\:]+\Z/
+         #     raise ArgumentError, "%s is not a valid value." % value
+          #  end
+          end
+        end
+      end
+    
+      newparam(:server_slot) do
+        desc "Name of service profile"
+        validate do |value|
+          if value.strip.length != 0
+           # unless value =~ /\A[a-zA-Z0-9\d_\.\:]+\Z/
+            #  raise ArgumentError, "%s is not a valid value." % value
+           # end
+          end
+        end
+      end
+    
+      newparam(:server_dn) do
+        desc "Name of service profile"
+        validate do |value|
+          if value.strip.length == 0
+            # if dn is empty then both org or profile name should exists.
+            if resource[:server_chesis_id].strip.length == 0 || resource[:server_slot].strip.length == 0
+              raise ArgumentError, "Either dn or both chesis id and server slot should be given in input."
+            end
+          else
+            #unless value =~ /\A[a-zA-Z0-9\d_\.\:]+\Z/
+             # raise ArgumentError, "%s is not a valid value." % value
+            #end
+          end
+        end
+      end
   
 end
