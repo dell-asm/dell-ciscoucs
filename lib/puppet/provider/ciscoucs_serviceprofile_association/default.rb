25fa4e978b11f37fd7479bbcb171362824030483
Ramesh Jha <Ramesh_Jha@dell.com>
2013-12-27 11:18:16 +0000

    Added changed xml creation method

--- lib/puppet/provider/ciscoucs_serviceprofile_association/default.rb
+++ lib/puppet/provider/ciscoucs_serviceprofile_association/default.rb
@@ -4,50 +4,97 @@ provider_path = Pathname.new(__FILE__).parent.parent
 Puppet.debug provider_path
 require File.join(provider_path, 'ciscoucs')
 
+
 Puppet::Type.type(:ciscoucs_serviceprofile_association).provide(:default, :parent => Puppet::Provider::Ciscoucs) do
   @doc = "Manage association of service profile on Cisco UCS device."
 
   include PuppetX::Puppetlabs::Transport
   @doc = "Associate server profile on Cisco UCS device."
   def create
-    
-    #@serverchesisid = resource[:serverchesisid]
-    @pndn = resource[:pndn]
-    #@serverslot = resource[:serverslot]      
-    @serviceprofilename = resource[:serviceprofilename]   
 
-      
+    name=resource[:name]
+
+    profile_dn = resource[:profile_dn]
+    if profile_dn == ""
+      profile_dn = 'org-'+resource[:organization_name]+'/ls-'+resource[:service_profile_name];
+    end
+
+    server_dn = resource[:server_dn]
+    if server_dn == ""
+      server_dn = 'sys/'+resource[:server_chesis_id]+'/'+resource[:server_slot];
+    end
+    
     formatter = PuppetX::Util::Ciscoucs::Xml_formatter.new("associateServiceProfile")
     parameters = PuppetX::Util::Ciscoucs::NestedHash.new
-
-    puts "---------befor xml"
     parameters['/configConfMos'][:cookie] = cookie
-    parameters['/configConfMos'][:inHierarchical] = 'false'
-    parameters['/configConfMos/inConfigs/pair'][:key] = @serviceprofilename
-    parameters['/configConfMos/inConfigs/pair/lsBinding'][:dn] = @serviceprofilename
-    parameters['/configConfMos/inConfigs/pair/lsBinding'][:pnDn] = @pndn
-    parameters['/configConfMos/inConfigs/pair/lsBinding'][:restrictMigration] = 'no'
-      
-    profile_associate_input_xml = formatter.command_xml(parameters)
-      
-    profile_associate_output_xml = post profile_associate_input_xml;
+    parameters['/configConfMos/inConfigs/pair'][:key] = profile_dn
+    parameters['/configConfMos/inConfigs/pair/lsServer'][:dn] = profile_dn
+    parameters['/configConfMos/inConfigs/pair/lsServer/lsBinding'][:pnDn] = server_dn
+    parameters['/configConfMos/inConfigs/pair/lsServer'][:descr] = "Service Profile Associated by ASM";
+    parameters['/configConfMos/inConfigs/pair/lsServer/lsBinding'][:rn] = "pn";
+    requestxml = formatter.command_xml(parameters);
     
-    assoresponse = REXML::Document.new(profile_associate_output_xml)
-    root = assoresponse.root
-    puts assoresponse
-      
+    if requestxml.to_s.strip.length == 0
+      raise Puppet::Error, "Cannot create request xml for asoociate operation"
+    end
+    responsexml = RestClient.post url, requestxml, :content_type => 'text/xml'
+    if responsexml.to_s.strip.length == 0
+      raise Puppet::Error, "No response obtained from Associate operation"
+    end
+    # Create an XML doc and parse it to get the cookie.
+    root = REXML::Document.new(responsexml).root
 
-
+    #if root.attributes['outCookie'].nil?
+    #  raise Puppet::Error, "Cannot obtain cookie from response"
+    #end
+    #@@cookie = root.attributes['outCookie']
   end
 
   def destroy
 
-    
+    name=resource[:name]
+    profile_dn = resource[:profile_dn]
+    if profile_dn == ""
+      profile_dn = 'org-'+resource[:organization_name]+'/ls-'+resource[:service_profile_name];
+    end
+
+    formatter = PuppetX::Util::Ciscoucs::Xml_formatter.new("disAssociateServiceProfile")
+    parameters = PuppetX::Util::Ciscoucs::NestedHash.new
+    parameters['/configConfMos'][:cookie] = cookie
+    parameters['/configConfMos/inConfigs/pair'][:key] = profile_dn
+    parameters['/configConfMos/inConfigs/pair/lsServer'][:dn] = profile_dn
+    parameters['/configConfMos/inConfigs/pair/lsServer/lsBinding'][:status] = "deleted";
+    parameters['/configConfMos/inConfigs/pair/lsServer'][:descr] = "Service Profile Dis Associated by ASM";
+    parameters['/configConfMos/inConfigs/pair/lsServer/lsBinding'][:rn] = "pn";
+    requestxml = formatter.command_xml(parameters);
+
+    if requestxml.to_s.strip.length == 0
+      raise Puppet::Error, "Cannot create request xml for Dis-Associate operation"
+    end
+    responsexml = RestClient.post url, requestxml, :content_type => 'text/xml'
+    if responsexml.to_s.strip.length == 0
+      raise Puppet::Error, "No response obtained from Dis-associate operation"
+    end
+    # Create an XML doc and parse it to get the cookie.
+    root = REXML::Document.new(responsexml).root
+
+    #if root.attributes['outCookie'].nil?
+    #  raise Puppet::Error, "Cannot obtain cookie from response"
+    #end
+    #@@cookie = root.attributes['outCookie']
+
   end
 
   #check If exist
   def exists?
-
+    ens = resource[:ensure]
+    result = false;
+    if (ens.to_s =="present")
+      result = false;
+    elsif (ens.to_s =="absent")
+      result = true;
+    end
+    return result;
   end
 
 end
