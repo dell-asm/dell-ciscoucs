3139b0f31daf47b5921e94721903e7ab37e30a8f
dilip panwar <dilip_panwar@dell.com>
2013-12-31 08:31:20 +0000

    done changes for create vlan in UCS

--- tests/create_vlan.pp
+++ tests/create_vlan.pp
@@ -1,18 +1,16 @@
 include ciscoucs
-
 transport_ciscoucs { 'ciscoucs':
   username => 'admin',
   password => 'admin',
   server   => '10.2.0.66',
 }
 
-
-create_vlan { 'testvlan':
-  ensure    => present,
-  transport  => Transport_ciscoucs['ciscoucs'],
-  id                => '10',
-  mcast_policy_name   => '',
-  policy_owner 		=> 'local',
-  sharing	        => 'none',
-  status 			=> 'created',
- }
+ciscoucs_create_vlan { 'testvlan':
+  ensure    		=> present,
+  transport 		=> Transport_ciscoucs['ciscoucs'],
+  id        		=> '10',
+  mcast_policy_name => '',
+  sharing	        => '',
+  fabric_id         => '',
+  status            => 'created',
+}
