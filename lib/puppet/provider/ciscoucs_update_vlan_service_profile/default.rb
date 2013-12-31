2f12105829aee499225420fadc0ccb7408b89437
subramaniam chandras <subramaniam_chandras@dell.com>
2013-12-31 04:50:11 +0000

    Update VLAN in Service Profile in Cisco UCS

---
+++ lib/puppet/provider/ciscoucs_update_vlan_service_profile/default.rb
@@ -0,0 +1,64 @@
+require 'pathname'
+
+provider_path = Pathname.new(__FILE__).parent.parent
+Puppet.debug provider_path
+require File.join(provider_path, 'ciscoucs')
+
+ucs_module = Puppet::Module.find('ciscoucs', Puppet[:environment].to_s)
+require File.join ucs_module.path, 'lib/puppet_x/util/ciscoucs/nested_hash'
+require File.join ucs_module.path, 'lib/puppet_x/util/ciscoucs/xml_formatter'
+
+Puppet::Type.type(:ciscoucs_update_vlan_service_profile).provide(:default, :parent => Puppet::Provider::Ciscoucs) do
+  include PuppetX::Puppetlabs::Transport
+  @doc = "Update VLAN in Service Profile Cisco UCS device."
+  def create
+    source_profile_dn = "#{@resource[:serviceprofileorg]}/ls-#{@resource[:name]}/ether-#{@resource[:vnic]}"
+    source_profile_rn = "if-#{resource[:vlanname]}"
+    formatter = PuppetX::Util::Ciscoucs::Xml_formatter.new("updateVLANServiceProfile")
+    parameters = PuppetX::Util::Ciscoucs::NestedHash.new
+    parameters['/configConfMos/inConfigs/pair/vnicEther'][:dn] = "#{source_profile_dn}"
+    parameters['/configConfMos/inConfigs/pair/vnicEther/vnicEtherIf'][:defaultNet] = @resource[:defaultnet]
+    parameters['/configConfMos/inConfigs/pair/vnicEther/vnicEtherIf'][:name] = @resource[:vlanname]
+    parameters['/configConfMos'][:cookie] = cookie
+    parameters['/configConfMos/inConfigs/pair'][:key] = "#{source_profile_dn}"
+    parameters['/configConfMos/inConfigs/pair/vnicEther/vnicEtherIf'][:rn] = "#{source_profile_rn}"
+    requestxml = formatter.command_xml(parameters)
+    if requestxml.to_s.strip.length == 0
+      raise Puppet::Error, "Cannot create request xml for update vlan service profile operation"
+    end
+    Puppet.debug "Sending update vlan service profile request xml: \n" + requestxml
+    responsexml = post requestxml
+    if responsexml.to_s.strip.length == 0
+      raise Puppet::Error, "No response obtained from  update vlan service profile"
+    end
+    Puppet.debug "Response from  update vlan service profile: \n" + responsexml
+    updatevlanidresponse = REXML::Document.new(responsexml)
+    root = updatevlanidresponse.root
+    updatevlanidresponse.elements.each("/configConfMos/outConfigs/pair/vnicEther") {
+      |e| 
+      if e.attributes["status"].eql?('modified')
+        Puppet.notice "Vlan updated successfully in Service Profile."
+      elsif e.attributes["status"].eql?('created')
+        Puppet.notice "Vlan updated successfully in newly created Service Profile."
+      else
+	raise Puppet::Error, "Unable to update VLAN in Service Profile"
+      end
+    }
+
+    if !root.attributes['errorDescr'].nil?
+      raise Puppet::Error, root.attributes['errorDescr']
+    end
+
+  end
+
+  def destroy
+    # not needed
+  end
+
+  def exists?
+    # check if the source profile exists
+      return false
+  end
+
+end
+
---
+++ lib/puppet/type/ciscoucs_update_vlan_service_profile.rb
@@ -0,0 +1,55 @@
+Puppet::Type.newtype(:ciscoucs_update_vlan_service_profile) do
+  @doc = 'Update VLAN in Service Profile Cisco UCS device.'
+  ensurable
+  newparam(:name) do
+    desc "Name of Service Profile"
+    validate do |value|
+      if value && value.strip.length != 0
+        unless value =~ /\A[a-zA-Z0-9\d_\.\:\-\/\s]{1,31}+\Z/
+          raise ArgumentError, "%s is not a valid name." % value
+        end
+      end
+    end
+  end
+
+  newparam(:vlanname) do
+    desc "Name of VLAN"
+    validate do |value|
+      if value && value.strip.length != 0
+        unless value =~ /\A[a-zA-Z0-9\d_\.\:\-\/\s]{1,31}+\Z/
+          raise ArgumentError, "%s is not a valid name." % value
+        end
+      end
+    end
+  end
+
+  newparam(:serviceprofileorg) do
+    desc "Name of Service Profile organization"
+    validate do |value|
+      if value && value.strip.length != 0
+        unless value =~ /\A[a-zA-Z0-9\d_\.\:\-\/\s]{1,31}+\Z/
+          raise ArgumentError, "%s is not a valid name." % value
+        end
+      end
+    end
+  end
+
+  newparam(:vnic) do
+    desc "Name of vNIC"
+    validate do |value|
+      if value && value.strip.length != 0
+        unless value =~ /\A[a-zA-Z0-9\d_\.\:\-\/\s]{1,31}+\Z/
+          raise ArgumentError, "%s is not a valid name." % value
+        end
+      end
+    end
+  end
+
+
+  newparam(:defaultnet) do
+    desc "Set Native VLAN"
+    newvalues(:yes, :no)
+  end
+
+end
+
