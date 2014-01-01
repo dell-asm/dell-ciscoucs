248f27dd323f35a5eb386a20c167b3d59186bd3f
Abhijit Kumar3 <Abhijit_Kumar3@dell.com>
2014-01-01 10:49:48 +0000

    data.pp file for externalizing all the input parameters of test pp files.

---
+++ tests/data.pp
@@ -0,0 +1,8 @@
+# Copyright (C) 2013 VMware, Inc.  All Rights Reserved.  All Rights Reserved.
+$ciscoucs = {
+  'username' => 'administrator',
+  'password' => 'iforgot@123',
+  'server'   => '172.16.100.167',
+  'options'  => { 'insecure' => true }
+}
+
