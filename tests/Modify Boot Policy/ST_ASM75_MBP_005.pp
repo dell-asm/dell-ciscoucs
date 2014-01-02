1cb32e492c59f3084cff43ef46652ee5d98491d2
GYAN KRISHNA <GYAN_KRISHNA@dell.com>
2014-01-02 15:00:18 +0000

    Automation test files for Modify Boot Policy.

---
+++ tests/Modify Boot Policy/ST_ASM75_MBP_005.pp
@@ -0,0 +1,23 @@
+include ciscoucs
+
+import '../data.pp'
+
+transport_ciscoucs { 'ciscoucs':
+  username => "${ciscoucs['username']}",
+  password => "${ciscoucs['password']}",
+  server   => "${ciscoucs['server']}",
+ 
+}
+
+
+ciscoucs_modify_serviceprofile_boot_policy { 'serviceprofilename':
+   ensure    => "${ciscoucs_modify_serviceprofile_boot_policy['modify']}",,
+   transport  => Transport_ciscoucs['ciscoucs'],
+   bootpolicydn => "#####",
+   bootpolicyname => "",
+   bootpolicyorganization => "",
+   serviceprofiledn => "{ciscoucs_modify_serviceprofile_boot_policy['serviceprofiledn']}",
+   serviceprofilename => "",
+   serviceprofileorganization => "",
+}
+
---
+++ tests/Modify Boot Policy/ST_ASM75_MBP_008.pp
@@ -0,0 +1,22 @@
+include ciscoucs
+
+import '../data.pp'
+
+transport_ciscoucs { 'ciscoucs':
+  username => "${ciscoucs['username']}",
+  password => "${ciscoucs['password']}",
+  server   => "${ciscoucs['server']}",
+ 
+}
+
+
+ciscoucs_modify_serviceprofile_boot_policy { 'serviceprofilename':
+   ensure    => "${ciscoucs_modify_serviceprofile_boot_policy['modify']}",,
+   transport  => Transport_ciscoucs['ciscoucs'],
+   bootpolicydn => "",
+   bootpolicyname => "{ciscoucs_modify_serviceprofile_boot_policy['bootpolicyname']}",
+   bootpolicyorganization => "{ciscoucs_modify_serviceprofile_boot_policy['bootpolicyorganization']}",
+   serviceprofiledn => "{$$]}",
+   serviceprofilename => "",
+   serviceprofileorganization => "",
+}
