011d8073613b27e230befc1658911d36c291e9b3
GYAN KRISHNA <GYAN_KRISHNA@dell.com>
2014-01-05 09:42:28 +0000

    Files added for associate and disassociate feature

---
+++ tests/AssociateDisassociate/disassociate041.pp
@@ -0,0 +1,22 @@
+include ciscoucs
+import '../data.pp'
+
+transport_ciscoucs { 'ciscoucs':
+  username => "${ciscoucs['username']}",
+  password => "${ciscoucs['password']}",
+  server   => "${ciscoucs['server']}",
+ 
+}
+
+ciscoucs_serviceprofile_association { 'name':
+  ensure    => "${ciscoucs_profile_association_dissociation['ensure_absent']}", 
+  organization_name => "${ciscoucs_profile_association_dissociation['organization_name']}",
+  service_profile_name => "${ciscoucs_profile_association_dissociation['orrt']}",
+  #server_chassis_id => "${ciscoucs_profile_association_dissociation['server_chassis_id']}",
+  #server_slot_id => "${ciscoucs_profile_association_dissociation['server_slot_id']}",   
+  
+  #profile_dn => "${ciscoucs_profile_association_dissociation['profile_dn']}",
+  server_dn => "${ciscoucs_profile_association_dissociation['server_dn']}",
+  
+  transport      => Transport_ciscoucs['ciscoucs'],
+}
