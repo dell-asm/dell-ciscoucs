cb2f7678c3a5e82c5dd36a0a0841e77fd40839be
GYAN KRISHNA <GYAN_KRISHNA@dell.com>
2014-01-06 07:03:32 +0000

    addednew test cases for service profile template,service profile by server.Deleted the earlier test cases.

---
+++ tests/createserviceprofilefromserver/createserviceprofile18.pp
@@ -0,0 +1,20 @@
+include ciscoucs
+import '../data.pp'
+
+transport_ciscoucs { 'ciscoucs':
+  username => "${ciscoucs['username']}",
+  password => "${ciscoucs['password']}",
+  server   => "${ciscoucs['server']}",
+ 
+}
+
+ciscoucs_serviceprofile { 'name':
+  name        => "${ciscoucs_serviceprofile['name']}",
+  org         => "${ciscoucs_serviceprofile['org']}",
+  #dn         => "  ",
+  server_chassis_id => "#",
+  server_slot => "${ciscoucs_serviceprofile['server_slot']}",
+  ensure          => "${ciscoucs_serviceprofile['ensure']}",
+  transport   => Transport_ciscoucs['ciscoucs'],
+}
+
---
+++ tests/createserviceprofilefromtemplate/profile_by_template_010.pp
@@ -0,0 +1,23 @@
+include ciscoucs
+
+import '../data.pp'
+
+# As a user I want to create Service Profile from Template.
+
+transport_ciscoucs { 'ciscoucs':
+  username => "${ciscoucs['username']}",
+  password => "${ciscoucs['password']}",
+  server   => "${ciscoucs['server']}",
+ 
+}
+
+
+ciscoucs_serviceprofile { 'name':
+  name        => " ",
+  org         => "${ciscoucs_serviceprofile['org']}",
+  #dn         => "${ciscoucs_serviceprofile['dn']}",
+  ensure  => "${ciscoucs_serviceprofile['ensure']}",
+  source_template => "${ciscoucs_serviceprofile['source_template']}",
+  transport   => Transport_ciscoucs['ciscoucs'],
+  number_of_profiles => "${ciscoucs_serviceprofile['number_of_profiles']}",
+}
