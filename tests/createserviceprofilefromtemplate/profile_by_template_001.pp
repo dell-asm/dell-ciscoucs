fcd73cc3dab8a072ecae3f8b466a2a33d48a4120
ravendra kumar <ravendra_kumar@dell.com>
2014-01-08 11:36:05 +0000

    checked in by Ankita Mishra, formatted pp files as per the puppet-lint standards.

--- tests/createserviceprofilefromtemplate/profile_by_template_001.pp
+++ tests/createserviceprofilefromtemplate/profile_by_template_001.pp
@@ -8,15 +8,14 @@ transport_ciscoucs { 'ciscoucs':
   username => "${ciscoucs['username']}",
   password => "${ciscoucs['password']}",
   server   => "${ciscoucs['server']}",
- 
 }
 
 
 ciscoucs_serviceprofile { 'serviceprofile_name':
-  serviceprofile_name        => "${ciscoucs_serviceprofile['serviceprofile_name']}",
-  organization         => "${ciscoucs_serviceprofile['organization']}",
-  #profile_dn         => "${ciscoucs_serviceprofile['profile_dn']}",
-  ensure  => "${ciscoucs_serviceprofile['ensure']}",
-  source_template => "${ciscoucs_serviceprofile['source_template']}",
-  transport   => Transport_ciscoucs['ciscoucs'],
+  serviceprofile_name   => "${ciscoucs_serviceprofile['serviceprofile_name']}",
+  organization          => "${ciscoucs_serviceprofile['organization']}",
+  #profile_dn           => "${ciscoucs_serviceprofile['profile_dn']}",
+  ensure                => "${ciscoucs_serviceprofile['ensure']}",
+  source_template       => "${ciscoucs_serviceprofile['source_template']}",
+  transport             => Transport_ciscoucs['ciscoucs'],
 }
