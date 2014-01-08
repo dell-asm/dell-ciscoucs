1896e35a5fcef55b09826b489fcd1d010b3a2a03
arpita agarwal <arpita_agarwal@dell.com>
2014-01-08 13:34:49 +0000

    Changes to ensure transport gets called from rspec

--- lib/puppet_x/puppetlabs/transport.rb
+++ lib/puppet_x/puppetlabs/transport.rb
@@ -8,11 +8,16 @@ module PuppetX
           res_ref = options[:resource_ref].to_s
           name = Puppet::Resource.new(nil, res_ref).title
           res_hash = catalog.resource(res_ref).to_hash
+          doconnect(res_hash)
         end
+
+      end
+
+      def self.doconnect(res_hash)
         transport = PuppetX::Puppetlabs::Transport::Ciscoucs.new(res_hash)
         transport.connect
         transport
-      end      
+      end
     end
   end
 end
