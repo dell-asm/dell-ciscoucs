7e4fe1e59b70c36dae0e5e231462a2d16a3b354d
richa sharma6 <richa_sharma6@dell.com>
2014-01-10 12:26:21 +0000

    Cheked in for puppet lint

--- tests/updatelanbootorder/updatelanbootorder029.pp
+++ tests/updatelanbootorder/updatelanbootorder029.pp
@@ -1,22 +1,18 @@
 include ciscoucs
+
 import '../data.pp'
 
 transport_ciscoucs { 'ciscoucs':
   username => "${ciscoucs['username']}",
   password => "${ciscoucs['password']}",
   server   => "${ciscoucs['server']}",
- 
 }
 
-
-ciscoucs_serviceprofile_boot_order{'bootpolicy_dn': 
-  bootpolicy_dn             => "$",  
-  transport      => Transport_ciscoucs['ciscoucs'],
-  ensure         => "${ciscoucs_serviceprofile_boot_order['ensure']}",
-  bootpolicy_name => "${ciscoucs_serviceprofile_boot_order['bootpolicy_name']}",
-  organization   => "${ciscoucs_serviceprofile_boot_order['organization']}",
-  lan_order       => "${ciscoucs_serviceprofile_boot_order['lan_order']}",
-
+ciscoucs_serviceprofile_boot_order{'bootpolicy_dn':
+  bootpolicy_dn         => "$",
+  transport             => Transport_ciscoucs['ciscoucs'],
+  ensure                => "${ciscoucs_serviceprofile_boot_order['ensure']}",
+  bootpolicy_name       => "${ciscoucs_serviceprofile_boot_order['bootpolicy_name']}",
+  organization          => "${ciscoucs_serviceprofile_boot_order['organization']}",
+  lan_order             => "${ciscoucs_serviceprofile_boot_order['lan_order']}",
 }
-
-
--- tests/updatelanbootorder/updatelanbootorder032.pp
+++ tests/updatelanbootorder/updatelanbootorder032.pp
@@ -1,22 +1,18 @@
 include ciscoucs
+
 import '../data.pp'
 
 transport_ciscoucs { 'ciscoucs':
   username => "${ciscoucs['username']}",
   password => "${ciscoucs['password']}",
   server   => "${ciscoucs['server']}",
- 
 }
 
-
-ciscoucs_serviceprofile_boot_order{'bootpolicy_dn': 
-  #bootpolicy_dn             => "${ciscoucs_serviceprofile_boot_order['bootpolicy_dn']}",  
-  transport      => Transport_ciscoucs['ciscoucs'],
-  ensure         => "${ciscoucs_serviceprofile_boot_order['ensure']}",
-  bootpolicy_name => "root1",
-  organization   => "${ciscoucs_serviceprofile_boot_order['organization']}",
-  lan_order       => "${ciscoucs_serviceprofile_boot_order['lan_order']}",
-
+ciscoucs_serviceprofile_boot_order{'bootpolicy_dn':
+  #bootpolicy_dn        => "${ciscoucs_serviceprofile_boot_order['bootpolicy_dn']}",
+  transport             => Transport_ciscoucs['ciscoucs'],
+  ensure                => "${ciscoucs_serviceprofile_boot_order['ensure']}",
+  bootpolicy_name       => "root1",
+  organization          => "${ciscoucs_serviceprofile_boot_order['organization']}",
+  lan_order             => "${ciscoucs_serviceprofile_boot_order['lan_order']}",
 }
-
-
