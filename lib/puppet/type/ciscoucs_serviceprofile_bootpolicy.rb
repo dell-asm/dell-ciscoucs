b11fd8d3394095d4febeb6a9140ab0331cc088b6
richa sharma6 <richa_sharma6@dell.com>
2014-01-10 10:27:02 +0000

    checkin for inline doc

--- lib/puppet/type/ciscoucs_serviceprofile_bootpolicy.rb
+++ lib/puppet/type/ciscoucs_serviceprofile_bootpolicy.rb
@@ -1,5 +1,5 @@
 Puppet::Type.newtype(:ciscoucs_serviceprofile_bootpolicy) do
-  @doc = 'Modify service profile boot policy on cisco ucs device'
+  @doc = 'This represents updation of service profile boot policy on cisco ucs'
   
   ensurable do
      newvalue(:modify) do
@@ -9,7 +9,7 @@ Puppet::Type.newtype(:ciscoucs_serviceprofile_bootpolicy) do
    end
 
  newparam(:serviceprofile_name, :namevar => true) do
-    desc "Name of the service profile"
+    desc "This parameter describes name of the service profile"
     validate do |value|
       if value && value.strip.length != 0
         unless value =~ /\A[a-zA-Z0-9\d_\.\:\-\/\s]{1,31}+\Z/
@@ -20,7 +20,7 @@ Puppet::Type.newtype(:ciscoucs_serviceprofile_bootpolicy) do
   end
 
   newparam(:serviceprofile_organization) do
-    desc "Name of the service profile organization. Valid format is org-root/[sub-organization]/[sub-organization]...."
+    desc "This parameter describes name of the service profile organization. A valid format is org-root/[sub-organization]/[sub-organization]...."
     validate do |value|
       if value && value.strip.length != 0
         unless value =~ /\A[a-zA-Z0-9\d_\.\:\-\/\s]{1,31}+\Z/
@@ -31,7 +31,7 @@ Puppet::Type.newtype(:ciscoucs_serviceprofile_bootpolicy) do
   end
 
   newparam(:serviceprofile_dn) do
-    desc "Source service profile DN .Valid format is org-root/[sub-organization]/[sub-organization]..../ls-[profile name]"
+    desc "This parameter describes source service profile dn. A valid format is org-root/[sub-organization]/[sub-organization]..../ls-[profile name]"
     validate do |value|
       if value  &&  value.strip.length != 0
         unless value =~ /\A[a-zA-Z0-9\d_\.\:\-\/\s]{1,31}+\Z/
@@ -42,7 +42,7 @@ Puppet::Type.newtype(:ciscoucs_serviceprofile_bootpolicy) do
   end
 
   newparam(:bootpolicy_name) do
-    desc "Name of the boot policy"
+    desc "This parameter describes name of the boot policy"
     validate do |value|
       if value && value.strip.length != 0
         unless value =~ /\A[a-zA-Z0-9\d_\.\:\-\/\s]{1,31}+\Z/
@@ -53,7 +53,7 @@ Puppet::Type.newtype(:ciscoucs_serviceprofile_bootpolicy) do
   end
 
   newparam(:bootpolicy_organization) do
-    desc "Name of boot policy organization "
+    desc "This parameter describes name of boot policy organization. A valid format is org-root/[sub-organization]/[sub-organization]...."
     validate do |value|
       if value && value.strip.length != 0
         unless value =~ /\A[a-zA-Z0-9\d_\.\:\-\/\s]{1,31}+\Z/
@@ -64,7 +64,7 @@ Puppet::Type.newtype(:ciscoucs_serviceprofile_bootpolicy) do
   end
 
   newparam(:bootpolicy_dn) do
-    desc "Boot policy DN. Valid format is org-root/boot-policy-[policy name]"
+    desc "This parameter describes Boot policy dn. A valid format is org-root/boot-policy-[policy name]"
     validate do |value|
       if value  &&  value.strip.length != 0
         unless value =~ /\A[a-zA-Z0-9\d_\.\:\-\/\s]{1,31}+\Z/
@@ -78,13 +78,13 @@ Puppet::Type.newtype(:ciscoucs_serviceprofile_bootpolicy) do
     if !self[:bootpolicy_dn] || self[:bootpolicy_dn].strip.length==0
       # if bootpolicydn is empty then both bootpolicyorganization and bootpolicyname should exists.
       if (!self[:bootpolicy_organization] || self[:bootpolicy_organization].strip.length == 0) || (!self[:bootpolicy_name] || self[:bootpolicy_name].strip.length == 0)
-        raise ArgumentError, "Either dn or both boot policy organization and policy name should be given."
+        raise ArgumentError, "Provide either dn or both boot policy organization and policy name as input."
       end
     end
     if !self[:serviceprofile_dn] || self[:serviceprofile_dn].strip.length==0
           # if serviceprofiledn is empty then both serviceprofileorganization and serviceprofilename should exists.
           if (!self[:serviceprofile_organization] || self[:serviceprofile_organization].strip.length == 0) || (!self[:serviceprofile_name] || self[:serviceprofile_name].strip.length == 0)
-            raise ArgumentError, "Either dn or both service profile organization and service profile name should be given."
+            raise ArgumentError, "Provide either dn or both service profile organization and service profile name as input."
           end
         end
         
