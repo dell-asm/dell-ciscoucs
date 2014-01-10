0c2c1eff6935137d5b8837510be7a5101351041d
ravendra kumar <ravendra_kumar@dell.com>
2014-01-10 09:02:38 +0000

    checked in by Ankita Mishra, formatted pp files as per the puppet-lint standards.

--- tests/createserviceprofilefromserver/createserviceprofile21.pp
+++ tests/createserviceprofilefromserver/createserviceprofile21.pp
@@ -1,20 +1,19 @@
 include ciscoucs
+
 import '../data.pp'
 
 transport_ciscoucs { 'ciscoucs':
   username => "${ciscoucs['username']}",
   password => "${ciscoucs['password']}",
   server   => "${ciscoucs['server']}",
- 
 }
 
 ciscoucs_serviceprofile { 'serviceprofile_name':
-  serviceprofile_name        => "${ciscoucs_serviceprofile['serviceprofile_name']}",
-  organization         => "${ciscoucs_serviceprofile['organization']}",
-  #profile_dn         => "  ",
-  server_chassis_id => " ",
-  server_slot => "${ciscoucs_serviceprofile['server_slot']}",
-  ensure          => "${ciscoucs_serviceprofile['ensure']}",
-  transport   => Transport_ciscoucs['ciscoucs'],
+  serviceprofile_name   => "${ciscoucs_serviceprofile['serviceprofile_name']}",
+  organization          => "${ciscoucs_serviceprofile['organization']}",
+  #profile_dn           => "  ",
+  server_chassis_id     => " ",
+  server_slot           => "${ciscoucs_serviceprofile['server_slot']}",
+  ensure                => "${ciscoucs_serviceprofile['ensure']}",
+  transport             => Transport_ciscoucs['ciscoucs'],
 }
-
--- tests/createserviceprofilefromserver/createserviceprofile27.pp
+++ tests/createserviceprofilefromserver/createserviceprofile27.pp
@@ -1,20 +1,19 @@
 include ciscoucs
+
 import '../data.pp'
 
 transport_ciscoucs { 'ciscoucs':
   username => "${ciscoucs['username']}",
   password => "${ciscoucs['password']}",
   server   => "${ciscoucs['server']}",
- 
 }
 
 ciscoucs_serviceprofile { 'serviceprofile_name':
-  serviceprofile_name        => "${ciscoucs_serviceprofile['serviceprofile_name']}",
-  organization         => "${ciscoucs_serviceprofile['organization']}",
-  #profile_dn         => "  ",
-  server_chassis_id => "${ciscoucs_serviceprofile['server_chassis_id']}",
-  server_slot => "orrt",
-  ensure          => "${ciscoucs_serviceprofile['ensure']}",
-  transport   => Transport_ciscoucs['ciscoucs'],
+  serviceprofile_name   => "${ciscoucs_serviceprofile['serviceprofile_name']}",
+  organization          => "${ciscoucs_serviceprofile['organization']}",
+  #profile_dn           => "  ",
+  server_chassis_id     => "${ciscoucs_serviceprofile['server_chassis_id']}",
+  server_slot           => "orrt",
+  ensure                => "${ciscoucs_serviceprofile['ensure']}",
+  transport             => Transport_ciscoucs['ciscoucs'],
 }
-
