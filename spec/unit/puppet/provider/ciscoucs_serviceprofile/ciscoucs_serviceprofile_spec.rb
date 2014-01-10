eebd6dd047133d8f2900d7e944b6e9f36ff1abb1
Abhijit Kumar3 <Abhijit_Kumar3@dell.com>
2014-01-10 10:31:23 +0000

    rspec for ciscoucs provider.

--- spec/unit/puppet/provider/ciscoucs_serviceprofile/ciscoucs_serviceprofile_spec.rb
+++ spec/unit/puppet/provider/ciscoucs_serviceprofile/ciscoucs_serviceprofile_spec.rb
@@ -10,8 +10,8 @@ describe Puppet::Type.type(:ciscoucs_serviceprofile).provider(:default) do
 
   before(:each) do
     @fixture = Ciscoucs_serviceprofile_poweron_fixture.new
-    mock_transport=double('transport')
-    @fixture.provider.transport = mock_transport
+   # mock_transport=double('transport')
+   # @fixture.provider.transport = mock_transport
   
     Puppet.stub(:debug)
   end
@@ -29,9 +29,7 @@ describe Puppet::Type.type(:ciscoucs_serviceprofile).provider(:default) do
       @fixture.provider.class.instance_method(:exists?).should_not == nil
     end
 
-    it "should have parent 'Puppet::Provider::Brocade_fos'" do
-      @fixture.provider.should be_kind_of(Puppet::Provider::Brocade_fos)
-    end
+   
   end
 
  
