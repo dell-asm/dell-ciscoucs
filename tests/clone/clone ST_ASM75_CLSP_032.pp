ae32714fde000f63415634c08592faa7798acdcd
ravendra kumar <ravendra_kumar@dell.com>
2014-01-13 10:55:00 +0000

    checked in by Ankita Mishra, formatted pp files as per the puppet-lint standards.

--- tests/clone/clone ST_ASM75_CLSP_032.pp
+++ tests/clone/clone ST_ASM75_CLSP_032.pp
@@ -1,23 +1,20 @@
 include ciscoucs
+
 import '../data.pp'
 
 transport_ciscoucs { 'ciscoucs':
   username => "${ciscoucs['username']}",
   password => "${ciscoucs['password']}",
   server   => "${ciscoucs['server']}",
- 
 }
 
 ciscoucs_serviceprofile_clone { 'sourceprofilename':
-   ensure         => "${ciscoucs_serviceprofile_clone['ensure']}",
-   transport  => Transport_ciscoucs['ciscoucs'],
-   #source_profile_dn => "${ciscoucs_serviceprofile_clone['source_profile_dn']}",
-   #target_profile_dn => "${ciscoucs_serviceprofile_clone['target_profile_dn']}", 
-   source_serviceprofile_name      => "${ciscoucs_serviceprofile_clone['source_serviceprofile_name']}",
-   source_organization => "${ciscoucs_serviceprofile_clone['source_organization']}",
-   target_serviceprofile_name      => "orrt",
-   target_organization => "${ciscoucs_serviceprofile_clone['target_organization']}",
+  ensure                      => "${ciscoucs_serviceprofile_clone['ensure']}",
+  transport                   => Transport_ciscoucs['ciscoucs'],
+  #source_profile_dn          => "${ciscoucs_serviceprofile_clone['source_profile_dn']}",
+  #target_profile_dn          => "${ciscoucs_serviceprofile_clone['target_profile_dn']}",
+  source_serviceprofile_name  => "${ciscoucs_serviceprofile_clone['source_serviceprofile_name']}",
+  source_organization         => "${ciscoucs_serviceprofile_clone['source_organization']}",
+  target_serviceprofile_name  => "orrt",
+  target_organization         => "${ciscoucs_serviceprofile_clone['target_organization']}",
 }
-
-
-
