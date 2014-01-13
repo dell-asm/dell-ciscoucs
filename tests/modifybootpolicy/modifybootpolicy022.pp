186c3d0676c420345a60d614f5f9de0886283711
richa sharma6 <richa_sharma6@dell.com>
2014-01-13 10:49:47 +0000

    Cheked in for puppet lint

--- tests/modifybootpolicy/modifybootpolicy022.pp
+++ tests/modifybootpolicy/modifybootpolicy022.pp
@@ -1,24 +1,20 @@
 include ciscoucs
+
 import '../data.pp'
 
 transport_ciscoucs { 'ciscoucs':
   username => "${ciscoucs['username']}",
   password => "${ciscoucs['password']}",
   server   => "${ciscoucs['server']}",
- 
 }
 
-ciscoucs_serviceprofile_bootpolicy{'serviceprofile_dn': 
-  #serviceprofile_dn =>"${ciscoucs_serviceprofile_bootpolicy['serviceprofile_dn']}",
-  transport      => Transport_ciscoucs['ciscoucs'],
-  ensure         => "${ciscoucs_serviceprofile_bootpolicy['ensure']}",
-  #bootpolicy_dn   => "${ciscoucs_serviceprofile_bootpolicy['bootpolicy_dn']}",
-  
-   bootpolicy_name => "${ciscoucs_serviceprofile_bootpolicy['bootpolicy_name']}",
-   bootpolicy_organization => "${ciscoucs_serviceprofile_bootpolicy['bootpolicy_organization']}",
-   
-   serviceprofile_name => " ",
-   serviceprofile_organization => "${ciscoucs_serviceprofile_bootpolicy['serviceprofile_organization']}",
+ciscoucs_serviceprofile_bootpolicy{'serviceprofile_dn':
+  ensure                      => "${ciscoucs_serviceprofile_bootpolicy['ensure']}",
+  #serviceprofile_dn          =>"${ciscoucs_serviceprofile_bootpolicy['serviceprofile_dn']}",
+  transport                   => Transport_ciscoucs['ciscoucs'],
+  #bootpolicy_dn              => "${ciscoucs_serviceprofile_bootpolicy['bootpolicy_dn']}",
+  bootpolicy_name             => "${ciscoucs_serviceprofile_bootpolicy['bootpolicy_name']}",
+  bootpolicy_organization     => "${ciscoucs_serviceprofile_bootpolicy['bootpolicy_organization']}",
+  serviceprofile_name         => " ",
+  serviceprofile_organization => "${ciscoucs_serviceprofile_bootpolicy['serviceprofile_organization']}",
 }
-
-
