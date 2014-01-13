d445c3496fc8fed72c75034546326fd909b9ff85
richa sharma6 <richa_sharma6@dell.com>
2014-01-13 06:20:42 +0000

    checked in for inline documentation

--- lib/puppet/type/ciscoucs_serviceprofile_boot_order.rb
+++ lib/puppet/type/ciscoucs_serviceprofile_boot_order.rb
@@ -1,10 +1,10 @@
 Puppet::Type.newtype(:ciscoucs_serviceprofile_boot_order) do
-  @doc = 'This represents updation of boot order in boot policy'
+  @doc = 'This represents the updation of Boot order in the Boot policy'
 
   ensurable
 
   newparam(:bootpolicy_name, :namevar => true ) do
-    desc "This parameter describes name of the boot policy"
+    desc "This parameter describes name of the Boot policy"
     validate do |value|
       if value && value.strip.length != 0
         unless value =~ /\A[a-zA-Z0-9\d_\.\:\-\/\s]{1,31}+\Z/
@@ -15,7 +15,7 @@ Puppet::Type.newtype(:ciscoucs_serviceprofile_boot_order) do
   end
 
   newparam(:bootpolicy_dn) do
-    desc "This parameter describes Boot policy dn. A valid format is org-root/boot-policy-[policy name]"
+    desc "This parameter describes Boot policy dn. A valid format for the Boot policy dn is org-root/boot-policy-[policy name]"
     validate do |value|
       if value  &&  value.strip.length != 0
         unless value =~ /\A[a-zA-Z0-9\d_\.\:\-\/\s]{1,31}+\Z/
@@ -26,7 +26,7 @@ Puppet::Type.newtype(:ciscoucs_serviceprofile_boot_order) do
   end
 
   newparam(:organization) do
-    desc "This parameter describes name of the boot policy organization. A valid format is org-root/[sub-organization]/[sub-organization]...."
+    desc "This parameter describes name of the Boot policy organization. A valid format for the Boot policy organization is org-root/[sub-organization]/[sub-organization]...."
     validate do |value|
       if value && value.strip.length != 0
         unless value =~ /\A[a-zA-Z0-9\d_\.\:\-\/\s]{1,31}+\Z/
@@ -37,7 +37,7 @@ Puppet::Type.newtype(:ciscoucs_serviceprofile_boot_order) do
   end
 
   newparam(:lan_order) do
-    desc "This parameter describes New LAN boot order which should be integer from 1 to 5"
+    desc "This parameter describes the New LAN Boot order, that must be an integer with a minimum value 1 and a maximum value 5"
     validate do |value|
       if value && value.strip.length != 0
         # value should be an integer from 1 to 5
@@ -45,7 +45,7 @@ Puppet::Type.newtype(:ciscoucs_serviceprofile_boot_order) do
           raise ArgumentError, "Allowed values for lan order is from 1 to 5"
         end
       else
-        raise ArgumentError, "Empty or no value given for lanorder parameter."
+        raise ArgumentError, "An empty or no value has been provided for the lanorder parameter."
       end
     end
   end
@@ -55,7 +55,7 @@ Puppet::Type.newtype(:ciscoucs_serviceprofile_boot_order) do
     if !self[:bootpolicy_dn] || self[:bootpolicy_dn].strip.length==0
       # if dn is empty then both org or profile name should exists.
       if (!self[:organization] || self[:organization].strip.length == 0) || (!self[:bootpolicy_name] || self[:bootpolicy_name].strip.length == 0)
-        raise ArgumentError, "Provide either dn or both organization and policy name as input."
+        raise ArgumentError, "Either a dn value or both the organization and policy name is to be provided as input."
       end
     end
   end
