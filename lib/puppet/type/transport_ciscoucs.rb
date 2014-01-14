e21a6ec6d0044b4a0d0165cd85e83dd31f8bb94e
arpita agarwal <arpita_agarwal@dell.com>
2014-01-14 13:42:41 +0000

    Fix for ASM-580: Cisco UCS - Error handling for invalid IPAddress in transport

--- lib/puppet/type/transport_ciscoucs.rb
+++ lib/puppet/type/transport_ciscoucs.rb
@@ -1,30 +1,36 @@
 Puppet::Type.newtype(:transport_ciscoucs) do
-  @doc = "Manage transport connectivity information on cisco ucs device"  
+  @doc = "Manage transport connectivity information on cisco ucs device"
   newparam(:name, :namevar => true) do
     desc "Name of the network transport."
   end
 
-  newparam(:username) do 
+  newparam(:username) do
     desc "User name for ciscoucs device"
     validate do |value|
-        if value && value.strip.length == 0            
-              raise ArgumentError, "Username can not be blank"         
-         end
-     end
+      if value && value.strip.length == 0
+        raise ArgumentError, "Username can not be blank"
+      end
+    end
   end
 
   newparam(:password) do
     desc "Password for ciscoucs device"
     validate do |value|
-          if value && value.strip.length == 0            
-              raise ArgumentError, "Password can not be blank"            
-          end
-        end
+      if value && value.strip.length == 0
+        raise ArgumentError, "Password can not be blank"
+      end
+    end
   end
-
   newparam(:server) do
     desc "Server ip for ciscoucs device"
-	defaultto('localhost')
+    validate do |value|
+      if value && value.strip.length == 0
+        raise ArgumentError, "IP Address can not be blank"
+      end
+      unless /\A(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})\Z/ =~ value
+        raise ArgumentError, "Invalid IP Address"
+      end
+    end
   end
 end
 
