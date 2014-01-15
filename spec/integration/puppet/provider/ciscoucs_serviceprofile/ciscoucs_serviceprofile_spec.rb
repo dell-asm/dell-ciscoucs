cc9176155d9ecb32ce35ac88c1397d2263310c45
Abhijit Kumar3 <Abhijit_Kumar3@dell.com>
2014-01-15 07:33:06 +0000

    Rspec integration test for create service profile from template and server

--- spec/integration/puppet/provider/ciscoucs_serviceprofile/ciscoucs_serviceprofile_spec.rb
+++ spec/integration/puppet/provider/ciscoucs_serviceprofile/ciscoucs_serviceprofile_spec.rb
@@ -59,6 +59,61 @@ describe Puppet::Type.type(:ciscoucs_serviceprofile).provider(:default) do
          
          )
        end  
+       
+  createprofilefromtemplate = ciscoucs_serviceprofile_yml['createfromtemplate'] 
+    
+  let :create_template do
+      @catalog = Puppet::Resource::Catalog.new
+            transport = Puppet::Type.type(:transport_ciscoucs).new({
+            :name => transport_node['name'],
+            :username => transport_node['username'],
+            :password => transport_node['password'],
+            :server   => transport_node['server'],
+          
+           })
+          @catalog.add_resource(transport)
+      
+        Puppet::Type.type(:ciscoucs_serviceprofile).new(
+          :name               => createprofilefromtemplate['name'],
+          :serviceprofile_name   => createprofilefromtemplate['serviceprofile_name'],
+          :transport          => transport, 
+          :catalog            => @catalog,
+          :organization       => createprofilefromtemplate['organization'],
+          :profile_dn         => createprofilefromtemplate['profile_dn'],
+          :ensure         => createprofilefromtemplate['ensure'],
+          :source_template         => createprofilefromtemplate['source_template'],
+          :number_of_profiles      => createprofilefromtemplate['number_of_profiles']
+          
+          )
+        end  
+        
+        
+        
+  createprofilefromserver = ciscoucs_serviceprofile_yml['createfromserver'] 
+     
+   let :create_server do
+       @catalog = Puppet::Resource::Catalog.new
+             transport = Puppet::Type.type(:transport_ciscoucs).new({
+             :name => transport_node['name'],
+             :username => transport_node['username'],
+             :password => transport_node['password'],
+             :server   => transport_node['server'],
+           
+            })
+           @catalog.add_resource(transport)
+       
+         Puppet::Type.type(:ciscoucs_serviceprofile).new(
+           :name               => createprofilefromserver['name'],
+           :serviceprofile_name   => createprofilefromserver['serviceprofile_name'],
+           :transport          => transport, 
+           :catalog            => @catalog,
+           :organization       => createprofilefromserver['organization'],
+           :profile_dn         => createprofilefromserver['profile_dn'],
+           :ensure         => createprofilefromserver['ensure'],
+           :server_chassis_id         => createprofilefromserver['server_chassis_id']
+
+           )
+         end  
  
    describe "when changing the power state of ciscoucs to up" do     
      it "should be able to change the state to up" do
@@ -71,4 +126,17 @@ describe Puppet::Type.type(:ciscoucs_serviceprofile).provider(:default) do
        power_off.provider.create
      end
    end
+   
+  describe "when creating server profile from template" do
+      it "it should able to create profile from template" do
+        create_template.provider.create 
+      end
+    end
+    
+  describe "when creating server profile from server" do
+      it "it should able to create profile from server" do
+        create_server.provider.create 
+      end
+    end
+   
  end
