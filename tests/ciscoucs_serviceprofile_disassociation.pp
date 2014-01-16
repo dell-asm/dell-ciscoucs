81c03d1ad4320f5eddcd3b2996cad69cb0512e90
GYAN KRISHNA <GYAN_KRISHNA@dell.com>
2014-01-16 13:49:17 +0000

    updated .pp file

--- tests/ciscoucs_serviceprofile_disassociation.pp
+++ tests/ciscoucs_serviceprofile_disassociation.pp
@@ -9,12 +9,12 @@ transport_ciscoucs { 'ciscoucs':
 }
 
 ciscoucs_serviceprofile_association { 'name':
-  ensure               => "${ciscoucs_profile_association_disassociation['ensure_absent']}",
-  organization         => "${ciscoucs_profile_association_disassociation['organization']}",
-  serviceprofile_name  => "${ciscoucs_profile_association_disassociation['serviceprofile_name']}",
-  profile_dn           => "${ciscoucs_profile_association_disassociation['profile_dn']}",
-  server_chassis_id    => "${ciscoucs_profile_association_disassociation['server_chassis_id']}",
-  server_slot_id       => "${ciscoucs_profile_association_disassociation['server_slot_id']}",
-  server_dn            => "${ciscoucs_profile_association_disassociation['server_dn']}",
+  ensure                => "${ciscoucs_profile_association_disassociation['ensure_absent']}",
+  organization          => "${ciscoucs_profile_association_disassociation['organization']}",
+  serviceprofile_name   => "${ciscoucs_profile_association_disassociation['serviceprofile_name']}",
+  profile_dn            => "${ciscoucs_profile_association_disassociation['profile_dn']}",
+  server_chassis_id     => "${ciscoucs_profile_association_disassociation['server_chassis_id']}",
+  server_slot_id        => "${ciscoucs_profile_association_disassociation['server_slot_id']}",
+  server_dn             => "${ciscoucs_profile_association_disassociation['server_dn']}",
   transport             => Transport_ciscoucs['ciscoucs'],
 }
